4c00c5dc726efe47a394e3d74fefb08eb569f91a
lanangwisana <lanangwisana@gmail.com>
2024-09-05 10:51:03 +0700

    create competitions table

---
+++ database/migrations/2024_09_05_034928_create_competitions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('competitions', function (Blueprint $table) {
+            $table->id();
+            $table->string('competition_name');
+            $table->string('year');
+            $table->string('organizer');
+            $table->string('achievement');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('competitions');
+    }
+};
