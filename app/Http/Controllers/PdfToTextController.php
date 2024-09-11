0fd6e8430a708026d92e515c35559d7e0bd52a17
lanangwisana <lanangwisana@gmail.com>
2024-09-11 13:23:38 +0700

    change controller PdfToTextController: extractText function

--- app/Http/Controllers/PdfToTextController.php
+++ app/Http/Controllers/PdfToTextController.php
@@ -29,14 +29,16 @@ class PdfToTextController extends Controller
 
         $extractedText = $text;
         $patternWorkExperience = '/Work experience\s*(?P<content>.*?)\s*(?=Projects|Project|$)/si';
-        $patternDetails = '/(?P<company>[^\n]+)\s*-\s*[^\n]*\s*(?P<start_date>[a-zA-Z]{3}(?:\s+\d{4})?)\s*-\s*(?P<end_date>[a-zA-Z]{3}(?:\s+\d{4})?)\s*(?P<position>[^\n]+)/';
+        $patternDetails = '/(?P<company>[^\n]+)\s*-\s*[^\n]*\s*(?P<start_date>[a-zA-Z]{3}(?:\s+\d{4})?)\s*–\s*(?P<end_date>[a-zA-Z]{3}\s+\d{4})\s*(?P<position>[^\n]+)/';
+        $patternDetails1 = '/(?P<company>[^\n]+)\s*-\s*[^\n]*\s*(?P<start_date>[a-zA-Z]{3})\s*-\s*(?P<end_date>[a-zA-Z]{3}\s+\d{4})\s*(?P<position>[^\n]+)/i';
 
+        
         if (preg_match($patternWorkExperience, $extractedText, $matches)) 
         {
             $workExperienceText = $matches['content'];
             if(preg_match_all($patternDetails, $workExperienceText, $matches, PREG_SET_ORDER)) 
             {
-                dd($matches);
+                // dd($matches);
                 foreach ($matches as $match) 
                 {
                     $position = trim($match['position']);
@@ -52,14 +54,34 @@ class PdfToTextController extends Controller
                         'start_date' => $startDate, 
                         'end_date' => $endDate
                     ]);
-                    // dd($match);
                 }
-            } else 
+            } 
+            if(preg_match_all($patternDetails1, $workExperienceText, $matches, PREG_SET_ORDER))
+            { 
+                // dd($matches);
+                foreach ($matches as $match) 
+                {
+                    $position = trim($match['position']);
+                    $company = trim($match['company']);
+                    $startDate = $match['start_date'];
+                    $endDate = $match['end_date'];
+
+                    // Simpan ke database
+                    ExtractedText::create
+                    ([
+                        'position' => $position, 
+                        'company' => $company, 
+                        'start_date' => $startDate, 
+                        'end_date' => $endDate
+                    ]);
+                }
+            } 
+            else 
             {
                 echo "Bagian Work Experience tidak ditemukan.";
             }
+        } 
         // Tampilkan teks yang diekstrak ke halaman
         return view('result', ['text' => $text]);
-        }
     }
 }
