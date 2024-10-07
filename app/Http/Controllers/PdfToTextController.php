355f70c29c8a52571bedf899b1738f951a859880
lanangwisana <lanangwisana@gmail.com>
2024-10-07 12:20:44 +0700

    update PdfToTextController: update private function extractCertificate

--- app/Http/Controllers/PdfToTextController.php
+++ app/Http/Controllers/PdfToTextController.php
@@ -274,14 +274,51 @@ class PdfToTextController extends Controller
                     $organizer = trim($match['organizer']);
                     $start_date = $match['start_date'];
                     $end_date = $match['end_date'];
-                    // Simpan ke database
-                    Certification::create
-                    ([
-                        'certification_name' => $certification_name, 
-                        'organizer' => $organizer, 
-                        'start_date' => $start_date, 
-                        'end_date' => $end_date
-                    ]);
+
+                    if(!preg_match('/^(?:\d{1,2}\s*)?[a-zA-Z]{3}\s+\d{4}$/', $end_date)){
+                        echo "End Date harus memiliki format tanggal MMM YYYY. \n";
+                        continue;
+                    }
+                    // Validasi start date
+                    if(preg_match('/^(?:\d{1,2}\s*)?[a-zA-Z]{3}\s+\d{4}$/', $start_date)){
+                        // Start date sudah sesuai dengan format MMM YYYY
+                        // Simpan ke database
+                        Certification::create
+                        ([
+                            'certification_name' => $certification_name, 
+                            'organizer' => $organizer, 
+                            'start_date' => $start_date, 
+                            'end_date' => $end_date
+                        ]);
+                    } elseif(preg_match('/^(?:\d{1,2}\s*)?[a-zA-Z]{3}$/', $start_date)){
+                        // Ambil tahun dari end date
+                        $endYear =(int)substr($end_date, -4);
+                        // Ambil bulan dari end_date
+                        $endMonth = substr($end_date, 0, 3);
+
+                        // Logika untuk menentukan tahun pada start_date
+                        $month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', "Jul", 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+                        $startMonthIndex = array_search($start_date, $month);
+                        $endMonthIndex = array_search($endMonth, $month);
+
+                        if ($startMonthIndex > $endMonthIndex) {
+                            $startYear = $endYear - 1;
+                        } else {
+                            $startYear = $endYear;
+                        }
+
+                        // Tambahkan tahun kepada start_date
+                        $start_date = $start_date . ' ' . $startYear;
+                        // Simpan ke database
+                        Certification::create
+                        ([
+                            'certification_name' => $certification_name, 
+                            'organizer' => $organizer, 
+                            'start_date' => $start_date, 
+                            'end_date' => $end_date
+                        ]);
+                    }
                 }
             }
         } 
