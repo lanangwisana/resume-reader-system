bb53ec7725d1ab9b17143278b0820104221477f3
lanangwisana <lanangwisana@gmail.com>
2024-10-07 09:16:23 +0700

    update PdfToTextController: update private function extractWorkExperience => add date validation

--- app/Http/Controllers/PdfToTextController.php
+++ app/Http/Controllers/PdfToTextController.php
@@ -35,34 +35,32 @@ class PdfToTextController extends Controller
         //Fungsi ekstraksi untuk Work Experience
         $this->extractWorkExperience($text);
         // Fungsi ekstraksi untuk Project
-        $this->extractProject($text);
+        // $this->extractProject($text);
         // Fungsi Ekstraksi untuk Competition
-        $this->extractCompetition($text); 
+        // $this->extractCompetition($text); 
         // Fungsi Ekstraksi untuk Certificate
-        $this->extractCertificate($text);
+        // $this->extractCertificate($text);
         // Tampilkan teks yang diekstrak ke halaman
         return view('result', ['text' => $text]);
     }
 
     private function extractWorkExperience($text){
         // Mencari data untuk begian work experience hingga sebelum bagian project
-        $patternWorkExperience = '/Work experience\s*(?P<content>.*?)\s*(?=Projects|Project|projects|project|Competitions|Competition|competitions|competition|Certificates|Certificate|certificates|certificate|Skills|Skill|skills|skill|$)/si';
+        $patternWorkExperience = '/Work experience\s*(?P<content>.*?)\s*(?=Projects|Project|projects|project|Competitions|Competition|competitions|competition|Certificates|Certificate|certificates|certificate|Skills|Skill|$)/s';
 
         if (preg_match($patternWorkExperience, $text, $matches)) 
         {
             $workExperienceText = $matches['content'];
-            dd($workExperienceText);
+            // dd($workExperienceText);
             
-            // Preprocessing menggunakan regex untuk memastikan company, start_date, dan end_date berada dalam satu baris
-            $pattern = '/(\d{1,2}\s*[a-zA-Z]{3})\s*\n\s*(\d{4})/';
-            $replacement = '$1 $2';
-            $workExperienceText = preg_replace($pattern, $replacement, $workExperienceText);
+            // $patternDetail = '/(?P<company>[^\n]+)\s*(?P<start_date>(?:\d{2})?\s*[a-zA-Z]{3}\s*(?:\d{4})?)\s*[-–]?\s*(?P<end_date>(?:\d{2})?\s*\n[a-zA-Z]{3}\s*\d{4})\s*\n(?P<position>[^\n]+)/';
+            // $patternDetail = '/(?P<company>[^\n]+)\s*(?P<start_date>[a-zA-Z]{3}(?:\s+\d{4})?)\s*-\s*(?P<end_date>[a-zA-Z]{3}\s+\d{4})\s*(?P<position>[^\n]+)/';
             // regex yang paling mendekati.
-            $patternDetail = '/(?P<company>[^\n]+)\s*(?:\t)?\s*(?P<start_date>(?:\d{1,2}\s*)?[a-zA-Z]{3}(?:\s+\d{4})?)\s*[-–]?\s*(?:\n\s*|\t)?(?P<end_date>(?:\d{1,2}\s*)?(?:\n\s*)?[a-zA-Z]{3}(?:\s+\d{4}|\n\s*\d{4}))\s*(?P<position>[^\n]+)/i';
+            $patternDetail = '/(?P<company>[^\n]+)\s*(?P<start_date>(?:\d{1,2}\s*)?[a-zA-Z]{3}(?:\s+\d{4})?)\s*[-–]?\s*(?:\n\s*|\t)?(?P<end_date>(?:\d{1,2}\s*)?(?:\n\s*)?[a-zA-Z]{3}(?:\s+\d{4}|\n\s*\d{4}))\s*\n(?P<position>[^\n]+)/i'; //Untuk regex ini sudah berhasil menangkap data tanggal denggan format dd MMM YYYY namun kedalanya adalah terdapat newline setelah tanggal pada bagian end_date sehingga memunculkan pola baru
 
             if(preg_match_all($patternDetail, $workExperienceText, $matches, PREG_SET_ORDER)) 
             {
-                // dd($matches);
+                dd($matches);
                 foreach ($matches as $match) 
                 {
                     $position = trim($match['position']);
@@ -70,18 +68,53 @@ class PdfToTextController extends Controller
                     $startDate = $match['start_date'];
                     $endDate = $match['end_date'];
 
-                    // Simpan ke database
-                    WorkExperience::create
-                    ([
-                        'position' => $position, 
-                        'company' => $company, 
-                        'start_date' => $startDate, 
-                        'end_date' => $endDate
-                    ]);
+                    // Validasi end date
+                    if(!preg_match('/^[a-zA-Z]{3}\s+\d{4}$/', $endDate)){
+                        echo "End Date harus memiliki format tanggal MMM YYYY. \n";
+                        continue;
+                    }
+                    // Validasi start date
+                    if(preg_match('/^[a-zA-Z]{3}\s+\d{4}$/', $startDate)){
+                        // Start date sudah sesuai dengan format MMM YYYY
+                        // Simpan ke database
+                        WorkExperience::create
+                        ([
+                            'position' => $position, 
+                            'company' => $company, 
+                            'start_date' => $startDate, 
+                            'end_date' => $endDate
+                        ]);
+                    } elseif(preg_match('/^[a-zA-Z]{3}$/', $startDate)){
+                        // Ambil tahun dari end date
+                        $endYear =(int)substr($endDate, -4);
+                        // Ambil bulan dari end_date
+                        $endMonth = substr($endDate, 0, 3);
+
+                        // Logika untuk menentukan tahun pada start_date
+                        $month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', "Jul", 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+                        $startMonthIndex = array_search($startDate, $month);
+                        $endMonthIndex = array_search($endMonth, $month);
+
+                        if ($startMonthIndex > $endMonthIndex) {
+                            $startYear = $endYear - 1;
+                        } else {
+                            $startYear = $endYear;
+                        }
+
+                        // Tambahkan tahun kepada start_date
+                        $startDate = $startDate . ' ' . $startYear;
+                        // Simpan ke database
+                        WorkExperience::create
+                        ([
+                            'position' => $position, 
+                            'company' => $company, 
+                            'start_date' => $startDate, 
+                            'end_date' => $endDate
+                        ]);
+                    } 
                 }
-            } 
-            else 
-            {
+            } else {
                 echo "Bagian Work Experience tidak ditemukan.";
             }
         }
