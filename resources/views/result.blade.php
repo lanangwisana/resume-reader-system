e8bd782151b2c0d4f6e6fb5e1176cd7fd067ce8b
lanangwisana <lanangwisana@gmail.com>
2024-10-22 19:17:53 +0700

    feat(alert): add alert in result

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -4,6 +4,16 @@
     <title>Extracted Text</title>
 </head>
 <body>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+            </ul>
+        </div>
+        @endif
+        
     <h2>Extracted Text from PDF</h2>
     <pre>{{ $text }}</pre>
 </body>
