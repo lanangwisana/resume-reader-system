90eedb260ba129fddb4fe46a75f0b491d42e849c
lanangwisana <lanangwisana@gmail.com>
2024-11-17 12:44:05 +0700

    simplify code

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>Extracted Text</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>    
     <h2>Extracted Text from PDF</h2>
@@ -21,5 +22,6 @@
         </div>
     @endif
     <pre>{{ $text }}</pre>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
